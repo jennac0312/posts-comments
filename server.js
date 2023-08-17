d939d9823c9a02bf3ec2a1513df38ae50531756c
Jenna C <jennac0312@gmail.com>
2023-08-17 14:43:45 -0400

    read route works! yay

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const Post = require("./models/Post")
+
 require("dotenv").config() // npm i dotenv ( environment variables )
 const mongoose = require('mongoose') // npm i mongoose
 const MONGO_URI = process.env.MONGO_URI // has tp be after dotenv
 
-// connect to database
+// MIDDLEWARE
+app.use(express.json({ extended: false })); // has to do with body parsing "everything will be parsed into json data"... puts req data in req.body ( sine post recieves data it needs this middleware )
+
+// CONNECT TO DATABASE
 mongoose.connect( MONGO_URI, {
     // rules to follow when using mongoose
     useNewUrlParser: true, // separated front end data into useable chunks for backend... workable format
@@ -16,12 +21,40 @@ mongoose.connection.once( 'open', () => {
     console.log( 'Connected to mongoDB' )
 })
 
-app.get( '/', ( req, res ) => {
-    res.send(`Testing testing`)
+// ROUTES 
+// app.get( '/', ( req, res ) => {
+//     res.send(`Testing testing`)
+// })
+
+// index route should be first
+// anytime youre going to database have to do capital model/schema, followed by verb
+app.get( '/', async ( req, res ) => {
+    try {
+        const allPosts = await Post.find( {} ) // creating a variable to poulate with all entities of the Post type
+        res.send(allPosts)
+    } catch (error) {
+        console.error(error)
+        // catch will always be server side error
+        res.status(500).json( { error: error.message } )
+    }
 })
 
+
 // C -> create. need to first make a model schema
-app.post( '/', ( req, res ) => {
+    //  every route that deals with database should have trycatch and sync/await. ( dont always need but good to have )
+app.post( '/', async ( req, res ) => {
+
+    try {
+       const post = await Post.create( req.body )
+       res.send( post )
+    } catch (error) {
+        console.error( error )
+        // res.status(500).json( { error: error.message } )
+    }
+
+    // TESTING
+        // - headers content-type application/json
+        // - body json { }
 
 })
 
