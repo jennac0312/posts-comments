b15030cb7ee35e2dd1a50c0e5ffd59d23de93c02
Jenna C <jennac0312@gmail.com>
2023-08-17 14:07:18 -0400

    connected to DB

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+require("dotenv").config() // npm i dotenv ( environment variables )
+const mongoose = require('mongoose') // npm i mongoose
+const MONGO_URI = process.env.MONGO_URI // has tp be after dotenv
+
+// connect to database
+mongoose.connect( MONGO_URI, {
+    // rules to follow when using mongoose
+    useNewUrlParser: true, // separated front end data into useable chunks for backend... workable format
+    useUnifiedTopology: true,
+})
+mongoose.connection.once( 'open', () => {
+    console.log( 'Connected to mongoDB' )
+})
 
 app.get( '/', ( req, res ) => {
     res.send(`Testing testing`)
